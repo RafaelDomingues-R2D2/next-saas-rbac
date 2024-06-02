81e6b00f2efe5d9e5c347df206a41daf714f1d24
Rafael Domingues-R2D2 <rafael.dominguesr2d2@gmail.com>
2024-06-02 09:55:10 -0300

    Permissoes pr cargos

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -6,7 +6,10 @@ import {
   MongoAbility,
 } from '@casl/ability'
 
-const actions = ['manage', 'invite'] as const
+import { User } from './models/user'
+import { permissions } from './permissions'
+
+const actions = ['manage', 'invite', 'delete'] as const
 const subjects = ['User', 'all'] as const
 type AppAbilities = [
   (typeof actions)[number],
@@ -19,8 +22,20 @@ export type AppAbility = MongoAbility<AppAbilities>
 
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
-const { build, can } = new AbilityBuilder(createAppAbility)
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
 
-can('invite', 'User')
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found`)
+  }
 
-export const ability = build()
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+
+  // can('invite', 'User')
+
+  // export const ability = build()
+}
